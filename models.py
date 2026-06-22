df0c82089dc48c3227edec0b29961863563c7071
Alden <ajyorba@gmail.com>
2026-06-22 18:45:46 -0400

    compute expected value of R_g(n) for subchains accross RW polymers

--- models.py
+++ models.py
@@ -19,11 +19,11 @@ def load_mmcif(filename: str)->PDB.Structure.Structure:
         return structure
 
 class CRW:
-    # class-level attributes: sphere diameter, spring constants
+    # class-level attributes: bead diameter, spring constants
     sigma = 1 # Backbone bead diameter
-    U_bb  = 1 # Bond length potential spring constant
+    U_bb = 1 # Bond length potential spring constant
     e_rep = 1 # Repulsive steric potential spring constant
-
+    mass = 1
     rng = np.random.default_rng()
 
     def __init__(self, n: int):
@@ -37,7 +37,6 @@ class CRW:
                 continue
             dr = self.rng.standard_normal(size=(3,))
             dr = dr / np.linalg.norm(dr)
-            count=0
             # current position is coords[i-1]+dr, while U_rep bewteen coords[i] and all previous beads (not including the one it's bonded to) 
             while(self.compute_U_rep(np.sqrt(np.sum((self.coords[i-1]+dr - self.coords[:i-1])**2, axis=-1))) > 0):
                 dr = self.rng.standard_normal(size=(3,))
@@ -46,15 +45,16 @@ class CRW:
             # res = scipy.optimize.minimize(fun=lambda r_i: self.compute_U_bond(sep=np.sqrt(np.sum((r_i-self.coords[i-1])**2))) + self.compute_U_rep(np.sqrt(np.sum((r_i - self.coords[:i-1])**2, axis=-1))), x0=self.coords[i-1]+dr)
             # print(f"({res.x[0]}, {res.x[1]}, {res.x[2]})")
             # self.coords[i]=res.x
-    
-    def show(self):
+
+    def show(self, verbose: bool=False):
         print(f"CRW of length {self.n}:\n\tSigma: {self.sigma}\n\tU_bb: {self.U_bb}\n\te_rep: {self.e_rep}")
         print("Stats:")
         print(f"\tRadius of Gyration: {self.compute_rg()}")
         print(f"\tTotal Potential Energy: {self.compute_U_tot()}")
         print("Residues:")
-        for i in range(self.n): 
-            print(f"\tBead {i}: ({self.coords[i,0]}, {self.coords[i,1]}, {self.coords[i,2]})")
+        for i in range(self.n):
+            if i<10 or verbose: 
+                print(f"\tBead {i}: ({self.coords[i,0]}, {self.coords[i,1]}, {self.coords[i,2]})")
 
     def compute_U_bond(self, sep: float):
         return np.sum(.5 * self.U_bb * (1-sep/self.sigma)**2)
@@ -85,6 +85,45 @@ class CRW:
             U_tot += self.compute_U_rep(dr)
 
         return U_tot / 2
+    
+    def write_sim_config(self, sim_config_filename: str):
+        with open(sim_config_filename, "w") as sim_file:
+            sim_file.write(self.get_sim_config())
+
+    def get_sim_config(self)->str:
+        atom_line_format_str = "ATOM,{},{},{},{},{},{},{}\nINTRA_RESIDUE\n"
+        bond_line_format_str = "INTER_RESIDUE,{},{},{},{}\n"
+        end_line_str = "END\n"
+        sim_string = ""
+        for i in range(self.n):
+            sim_string += atom_line_format_str.format(i, i, self.coords[i][0], self.coords[i][1], self.coords[i][2], self.sigma, self.mass)
+        for i in range(self.n-1):
+            sim_string += bond_line_format_str.format(i, i+1, self.U_bb, self.sigma)
+        sim_string += end_line_str
+        return sim_string
+
+    def collapse(self, central_force: float):
+        # write to sim file
+        # use subprocess to call polymer --simtype collapse_polymer --dt (? what should dt be) --damping 0
+        # --initial-temp (? what should initial temp be) --in ../../input/ --infile {sim_file} 
+        # --out ../../output/{sim_file} --cf-mag (? what should cf-mag be)
+        pass
+
+class BADA:
+    pass
+
+class FJSC:
+    pass
+
+class InSeq:
+    pass
+
+class MPSC:
+    pass
+
+class ModMPSC:
+    pass
+
 
 def norm(x: np.ndarray):
     return np.linalg.norm(x)
@@ -92,3 +131,4 @@ def norm(x: np.ndarray):
 if __name__=="__main__":
     my_crw = CRW(100)
     my_crw.show()
+    my_crw.write_sim_config("../../input/crw_100.txt")
